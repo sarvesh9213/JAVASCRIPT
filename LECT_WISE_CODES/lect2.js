9d0d9e2fc00ce0afdb95314afb4fa5e9890dea5c
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-23 23:40:15 +0530

    Create lect2.js

---
+++ LECT_WISE_CODES/lect2.js
@@ -0,0 +1,7 @@
+let a = 67;
+console.log(a);
+a = " sarvesh pandey ";
+console.log(a);
+// 67
+// sarvesh pandey
+// this shows that ki bhyii aap change ker sekte ho hrr baar string bhi daaal sekte ho
