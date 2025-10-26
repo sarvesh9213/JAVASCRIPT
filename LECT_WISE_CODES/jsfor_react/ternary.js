bc323eb2e9e83bf49d4a7c0d6759885716ad2766
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-26 15:54:35 +0530

    Update ternary.js

--- LECT_WISE_CODES/jsfor_react/ternary.js
+++ LECT_WISE_CODES/jsfor_react/ternary.js
@@ -1,3 +1,6 @@
-const component = () =>{
-  return age >10 ? div " pedro " /div :  div "jack" /div ;
-}
+let age = 16;
+const Component1 = () => {
+  let name = age > 10 ? <div>Pedro</div> : <div>Jack</div>;
+  return name;
+};
+const Component2 = () => (age > 10 ? <div>Pedro</div> : <div>Jack</div>);
