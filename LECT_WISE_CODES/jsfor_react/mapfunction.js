96799cee3c0b86174e568f832f0bd19bb0973466
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-26 16:49:15 +0530

    Update mapfunction.js

--- LECT_WISE_CODES/jsfor_react/mapfunction.js
+++ LECT_WISE_CODES/jsfor_react/mapfunction.js
@@ -1,10 +1,10 @@
 // Create an array of names
-let names = ["Pedro", "Jessica", "Carol"];
+let names1 = ["Pedro", "Jessica", "Carol"];
 
 // Use the .map() method to create a new array
 // .map() takes a callback function and runs it on every element of the array
 // It always returns a new array with the results of the callback
-names.map((name) => {
+names1.map((name) => {
   // This callback receives each element (here called 'name') one by one
   // For every element, it returns the string "Joe"
   return "Joe";
@@ -17,3 +17,14 @@ names.map((name) => {
 
 // Result: a new array is created (original array 'names' stays the same)
 ["Joe", "Joe", "Joe"];
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+////////////////////////// BEST WAY ///////////////////////////////////////////////
+let names2 = ["Pedro", "Jessica", "Carol"];
+
+// .map() creates a new array by running the function on each element
+let newnames = names2.map((name) => {
+  return "Joe"; // every element is replaced with "Joe"
+});
+
+// Print the new array
+console.log(newnames); // ["Joe", "Joe", "Joe"]
